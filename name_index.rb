bed36522fa8eead12af22c23942f8616c2ca6ab4
Koichi-Kobayashi <koichi.code@gmail.com>
2014-05-31 08:05:27 +0900

    refactoring

--- name_index.rb
+++ name_index.rb
@@ -4,9 +4,14 @@ class NameIndex
     ary = %w[ア カ サ タ ナ ハ マ ヤ ラ ワ ン]
     ret_ary = []
 
-    names.group_by{|n| ary[(ary.index{|i| i > n[0]} - 1)]}.each do |k, v|
-      ret_ary.push(k, v.sort)
+    # namesの各要素をnに入れながら、nの1文字目より大きいaryの各要素を検索する
+    # 見付かればその位置（aryを昇順に検索するので-1した位置）にあるaryの文字毎にグループ分けする
+    # .to_iとしているのは、nilの場合に落ちないようにしている（ary.indexの戻り値を0に変換している）
+    # ary[-1] == 'ン'だった！
+    names.group_by{|n| ary[(ary.index{|i| i > n[0]}.to_i - 1)]}.each do |k, v|
+      ret_ary.push(Array[k, v.sort])
     end
+    ret_ary.sort!{|a, b| a[0] <=> b[0]}
 
     return ret_ary
 
--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -1,6 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 
 describe NameIndex do
-  specify { expect(NameIndex.create_index(['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ'])).to eq ['カ', ['カネダ', 'キシモト'], 'ア', ['イトウ'], 'ハ', ['ハマダ', 'ババ'], 'ワ', ['ワダ']] }
+  specify { expect(NameIndex.create_index(['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ'])).to eq [['ア', ['イトウ']], ['カ', ['カネダ', 'キシモト']], ['ハ', ['ハマダ', 'ババ']], ['ワ', ['ワダ']]] }
+  specify { expect(NameIndex.create_index(['ゴトウ', 'アキモト', 'コバヤシ', 'モリモト', 'ンネサカ', 'ワタナベ'])).to eq [['ア', ['アキモト']], ['カ', ['コバヤシ', 'ゴトウ']], ['マ', ['モリモト']], ['ワ', ['ワタナベ']], ['ン', ['ンネサカ']]] }
+
 end
 
